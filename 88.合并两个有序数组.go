7572d4eebb138bd044386988a67930c3758476dc
silentdragon <22919zpw@gmail.com>
2023-05-05 16:16:27 +0800

    [88] 合并两个有序数组

---
+++ 88.合并两个有序数组.go
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=golang
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	nums1 = append(nums1[:m], nums2...)
+	sort.Ints(nums1)
+}
+
+// @lc code=end
+
